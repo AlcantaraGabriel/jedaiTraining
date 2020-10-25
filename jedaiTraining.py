6570aaecb60fc430b2eaf7db03121cd87ef4bc68
Gabriel Alcantara <gabrielalcantara39@gmail.com>
2020-10-25 12:41:57 -0300

    inserindo audio de colisão
    colisão covid com carrinho
    funções de audio

--- jedaiTraining.py
+++ jedaiTraining.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+from playsound import playsound
 
 #iniciando pygame
 pygame.init()
@@ -11,7 +12,11 @@ pygame.display.set_caption("JedaiTraining")
 icon = pygame.image.load('g851.png')
 pygame.display.set_icon(icon)
 
+font  = pygame.font.Font('Arial.ttf',20)
 
+def showAtenMed():
+    valores = font.render("Valor Meditação: \nValor Atenção: ",True,(255,255,255))
+    screen.blit(valores,(20,20))
 
 class P_cariribot:
     def __init__(self):
@@ -37,6 +42,14 @@ class P_cariribot:
             self.at = 0
         else:
             self.at = 1
+            
+    def som(self): 
+        pygame.mixer.music.load('infectado.ogg')
+        pygame.mixer.music.play(1)
+
+    def som2(self): 
+        pygame.mixer.music.load('eliminado.ogg')
+        pygame.mixer.music.play(1)
 
 class P_covid:
     def __init__(self):
@@ -78,13 +91,18 @@ class proteCovid:
     def resetPos(self):
         self.p_X = 10
         self.p_Y = 460
+
+    def som(self): 
+        pygame.mixer.music.load('fire.ogg')
+        pygame.mixer.music.play(1)
+       
         
 #playerImg1 = pygame.image.load('g852.png')
 #playerImg2 = pygame.image.load('g853.png')
 #playerImg3 = pygame.image.load('covid.png')
 
 
-
+    
 condicao = True
 at = 0
 
@@ -113,6 +131,7 @@ while condicao:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
+                alcool.som()
                 alcool.velocity = 0
     if(caririBot.at==0):
         caririBot.setImg('g853.png')
@@ -125,20 +144,27 @@ while condicao:
         
         if(alcool.p_Y >= covid19.p_Y and alcool.p_Y <= covid19.p_Y+55 and alcool.p_X >= covid19.p_X and alcool.p_X <= covid19.p_X+55):
             alcool.velocity = None
+            caririBot.som2()
             alcool.resetPos()
             covid19.resetPos()
         elif(alcool.p_Y+58 >= covid19.p_Y and alcool.p_Y+58 <= covid19.p_Y+55 and alcool.p_X+91 >= covid19.p_X and alcool.p_X+91 <= covid19.p_X+55): 
             alcool.velocity = None
+            caririBot.som2()
             alcool.resetPos()
             covid19.resetPos()
         else:
             alcool.setDisplay()
             alcool.p_X +=2
             
+    if(covid19.p_X<=caririBot.p_X+5):
+        caririBot.som()
+        covid19.resetPos()
+            
+            
     caririBot.setDisplay()
     covid19.setDisplay()
     covid19.rotat()
-
+    showAtenMed()
     pygame.display.update()
     
     
